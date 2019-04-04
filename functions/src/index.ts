0a30d5a7ae3e3b24b051175bc142fd99965a4a67
Phil A <phil.armstrong1@gmail.com>
2019-04-04 11:33:51 +0100

    Create user function and delete marker functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,13 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+export const onCreateUser = functions.auth.user()
+    .onCreate(user => admin.firestore().collection('/users').doc(user.uid).set({ displayName: user.displayName, id: user.uid }));
+
+export const onDeleteMapDeleteAllMarkers = functions.firestore.document('/users/{uid}/maps/{mapId}')
+    .onDelete((snapshot, context) => admin.firestore().collection(`/users/${context.auth!.uid}/markers`)
+        .where('mapId', '==', snapshot.data()!.id)
+        .get()
+        .then(results => results.forEach(result => result.ref.delete())));
