6007a6710a844e8f431037333cd82e6ddba1c763
Phil A <phil.armstrong1@gmail.com>
2019-04-04 11:41:08 +0100

    Adding logging to delete function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,7 +14,12 @@ export const onCreateUser = functions.auth.user()
     .onCreate(user => admin.firestore().collection('/users').doc(user.uid).set({ displayName: user.displayName, id: user.uid }));
 
 export const onDeleteMapDeleteAllMarkers = functions.firestore.document('/users/{uid}/maps/{mapId}')
-    .onDelete((snapshot, context) => admin.firestore().collection(`/users/${context.auth!.uid}/markers`)
-        .where('mapId', '==', snapshot.data()!.id)
-        .get()
-        .then(results => results.forEach(result => result.ref.delete())));
+    .onDelete((snapshot, context) => {
+        console.log(`Deleting markers for map id: ${snapshot.data()!.id}`);
+        return admin.firestore().collection(`/users/${context.auth!.uid}/markers`)
+            .where('mapId', '==', snapshot.data()!.id).get()
+            .then(results => {
+                console.log(`Markers for map: ${results.size}`);
+                results.forEach(result => result.ref.delete());
+            });
+    });
